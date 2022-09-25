38a95f340c4718a3a3f31f37d082a7fd0cd88396
dsangule <dsangule@gmail.com>
2022-09-25 14:24:20 +0530

    Section 10 formatting

--- cpp/codes/Section_10_Challenge_Assignent/main.cpp
+++ cpp/codes/Section_10_Challenge_Assignent/main.cpp
@@ -11,19 +11,15 @@ int main(){
     cin >> letter;
     
     for(size_t center = 0; center < letter.size(); center++){
-        
         size_t spaces = letter.size() - center - 1;
         while(spaces > 0){
             cout << " ";
             spaces--;
         }
-        
         for(size_t i = 0; i<center ; i++){
             cout << letter[i];
         }
-        
         cout << letter[center];
-        
         for(size_t i = 0; i<center ; i++){
             cout << letter[center-i-1];
         }
